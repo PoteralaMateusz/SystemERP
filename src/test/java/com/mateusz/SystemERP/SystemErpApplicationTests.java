9399a8582c92dc38dc8e0b1d672811da8079388f
MateuszPoterala <poteralamateusz@gmail.com>
2023-04-29 19:46:17 +0200

    init

---
+++ src/test/java/com/mateusz/SystemERP/SystemErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusz.SystemERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
