34b570fb2c5ebf41eb03b2df8119da8d385f0203
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-10 18:56:54 +0200

    add column pieces in product entity, some changes

--- src/main/java/com/mateusz/SystemERP/model/product/Product.java
+++ src/main/java/com/mateusz/SystemERP/model/product/Product.java
@@ -3,16 +3,13 @@ package com.mateusz.SystemERP.model.product;
 import com.mateusz.SystemERP.model.order.Order;
 import com.mateusz.SystemERP.model.item.Item;
 import jakarta.persistence.*;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @NoArgsConstructor
-@Setter
-@Getter
+
 @Table(name = "products")
 public class Product {
 
@@ -20,15 +17,65 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String drawingName;
+    private String pieces;
+    private double totalWeight;
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
     @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL,mappedBy = "product")
     private List<Item> items;
-    private double totalWeight;
+
 
     public Product(Order order, double totalWeight) {
         this.order = order;
         this.totalWeight = totalWeight;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDrawingName() {
+        return drawingName;
+    }
+
+    public void setDrawingName(String drawingName) {
+        this.drawingName = drawingName;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    private List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public double getTotalWeight() {
+        return totalWeight;
+    }
+
+    public void setTotalWeight(double totalWeight) {
+        this.totalWeight = totalWeight;
+    }
+
+    public String getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(String pieces) {
+        this.pieces = pieces;
     }
 }
