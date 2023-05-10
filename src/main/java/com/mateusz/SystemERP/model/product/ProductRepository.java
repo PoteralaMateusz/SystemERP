7f3e314ddeb10f2e022ad3cc62969c28ef14c91a
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-10 20:19:17 +0200

    add findProductById to repository

--- src/main/java/com/mateusz/SystemERP/model/product/ProductRepository.java
+++ src/main/java/com/mateusz/SystemERP/model/product/ProductRepository.java
@@ -3,10 +3,13 @@ package com.mateusz.SystemERP.model.product;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository {
 
     List<Product> findAll();
+
+    Optional<Product> findProductById(Long id);
 
     List<Product> findProductsByOrderId(Long id);
 
--- src/main/java/com/mateusz/SystemERP/model/product/SqlProductRepository.java
+++ src/main/java/com/mateusz/SystemERP/model/product/SqlProductRepository.java
@@ -8,11 +8,14 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SqlProductRepository extends ProductRepository, JpaRepository<Product, Long> {
 
     List<Product> findProductsByOrderId(Long id);
+
+    Optional<Product> findProductById(Long id);
 
     @Modifying
     @Transactional
