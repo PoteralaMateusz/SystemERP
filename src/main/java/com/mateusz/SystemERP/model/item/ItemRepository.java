3f56f1562ff4d5f0f27907806eb1b6b89121a4a0
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-10 20:20:09 +0200

    add findAll and addItemWithProductId to repository

--- src/main/java/com/mateusz/SystemERP/model/item/ItemRepository.java
+++ src/main/java/com/mateusz/SystemERP/model/item/ItemRepository.java
@@ -1,11 +1,15 @@
 package com.mateusz.SystemERP.model.item;
 
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ItemRepository {
 
+    List<Item> findAll();
+
     List<Item> findItemsByProductId(Long id);
 
-    Item save(Item item);
+    void addItemWithProductId(String material, int pieces, String quality, double weight, Long productId);
 
 }
--- src/main/java/com/mateusz/SystemERP/model/item/SqlItemRepository.java
+++ src/main/java/com/mateusz/SystemERP/model/item/SqlItemRepository.java
@@ -1,13 +1,28 @@
 package com.mateusz.SystemERP.model.item;
 
+import com.mateusz.SystemERP.model.product.Product;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface SqlItemRepository extends ItemRepository,JpaRepository<Item,Long> {
+public interface SqlItemRepository extends ItemRepository, JpaRepository<Item, Long> {
 
     List<Item> findItemsByProductId(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO ITEMS (MATERIAL, PIECES, QUALITY, WEIGHT, PRODUCT_ID) " +
+            "VALUES (:material,:pieces,:quality,:weight,:productId )", nativeQuery = true)
+    void addItemWithProductId(@Param("material") String material,
+                              @Param("pieces") int pieces,
+                              @Param("quality") String quality,
+                              @Param("weight") double weight,
+                              @Param("productId") Long productId);
 
 }
