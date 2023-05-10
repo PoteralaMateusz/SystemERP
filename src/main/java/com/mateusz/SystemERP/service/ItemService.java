7adb01706df3f5c78b987537ed1b96c4c14f124c
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-10 20:21:22 +0200

    add findAllItems and addItemWithProductId to service

--- src/main/java/com/mateusz/SystemERP/service/ItemService.java
+++ src/main/java/com/mateusz/SystemERP/service/ItemService.java
@@ -2,19 +2,35 @@ package com.mateusz.SystemERP.service;
 
 import com.mateusz.SystemERP.model.item.Item;
 import com.mateusz.SystemERP.model.item.ItemRepository;
+import com.mateusz.SystemERP.model.product.Product;
+import com.mateusz.SystemERP.model.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
-    private final ItemRepository repository;
+    private final ItemRepository itemRepository;
+    private final ProductRepository productRepository;
+
+    public ResponseEntity<List<Item>> findAllItems(){
+        List<Item> items = itemRepository.findAll();
+        if (items.isEmpty()){
+            return ResponseEntity
+                    .status(404)
+                    .build();
+        }
+        return ResponseEntity
+                .status(200)
+                .body(items);
+    }
 
     public ResponseEntity<List<Item>> findItemsByProductId(Long id){
-        List<Item> itemsByProductId = repository.findItemsByProductId(id);
+        List<Item> itemsByProductId = itemRepository.findItemsByProductId(id);
         if (itemsByProductId.isEmpty()) {
             return ResponseEntity
                     .status(404)
@@ -25,16 +41,17 @@ public class ItemService {
                 .body(itemsByProductId);
     }
 
-    public ResponseEntity<?> addItem(Item toAdd){
-        if (toAdd == null){
+    public ResponseEntity<?> addItemWithProductId(Long productId, Item toAdd){
+        Optional<Product> product = productRepository.findProductById(productId);
+        if (product.isEmpty() || toAdd == null){
             return ResponseEntity
-                    .status(400)
+                    .status(404)
                     .build();
         }
-        toAdd.setId(null);
+        itemRepository.addItemWithProductId(toAdd.getMaterial(),toAdd.getPieces(),toAdd.getQuality(), toAdd.getWeight(), productId);
         return ResponseEntity
                 .status(200)
-                .body(repository.save(toAdd));
+                .build();
 
     }
 }
