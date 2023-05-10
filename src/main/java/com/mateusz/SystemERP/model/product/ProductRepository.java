803e497fd532d3e68b51709d0cb71c96f1b0c9f8
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-10 18:57:40 +0200

    add addProductWithOrderId (modifying query)

--- src/main/java/com/mateusz/SystemERP/model/product/ProductRepository.java
+++ src/main/java/com/mateusz/SystemERP/model/product/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.mateusz.SystemERP.model.product;
 
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ProductRepository {
 
-    Product save(Product product);
+    List<Product> findAll();
+
     List<Product> findProductsByOrderId(Long id);
+
+    void addProductWithOrderId(String drawingName, String quality, double totalWeight, long orderID);
 }
--- src/main/java/com/mateusz/SystemERP/model/product/SqlProductRepository.java
+++ src/main/java/com/mateusz/SystemERP/model/product/SqlProductRepository.java
@@ -1,6 +1,10 @@
 package com.mateusz.SystemERP.model.product;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +13,14 @@ import java.util.List;
 public interface SqlProductRepository extends ProductRepository, JpaRepository<Product, Long> {
 
     List<Product> findProductsByOrderId(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO PRODUCTS (DRAWING_NAME,PIECES,TOTAL_WEIGHT,ORDER_ID)" +
+            "VALUES (:drawingName,:pieces,:totalWeight,:orderID)", nativeQuery = true)
+    void addProductWithOrderId(@Param("drawingName") String drawingName,
+                               @Param("pieces") String pieces,
+                               @Param("totalWeight") double totalWeight,
+                               @Param("orderID") long orderID);
 
 }
