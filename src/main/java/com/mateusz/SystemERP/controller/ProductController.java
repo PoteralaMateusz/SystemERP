2282bf8e7d88a215f298dcf29a2f457f980ebc75
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-10 18:58:52 +0200

    add createProductByOrderId and findAllProducts in controller

--- src/main/java/com/mateusz/SystemERP/controller/ProductController.java
+++ src/main/java/com/mateusz/SystemERP/controller/ProductController.java
@@ -13,13 +13,18 @@ import java.util.List;
 public class ProductController {
     private final ProductService service;
 
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> findAllProducts(){
+        return service.findAllProducts();
+    }
+
     @GetMapping("/products/{orderId}")
     public ResponseEntity<List<Product>> findProductByOrderId(@PathVariable Long orderId){
-        return service.findProductByOrderId(orderId);
+        return service.findProductsByOrderId(orderId);
     }
 
     @PostMapping("/products/{orderId}")
     public ResponseEntity<Product> createProductByOrderId(@PathVariable Long orderId , @RequestBody Product product){
-        return service.createProductByOrderId(orderId,product);
+        return service.createProductWithOrderId(product, orderId);
     }
 }
