a7a29114b1406e134ab5fbcc52a7b8384cc66ef6
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-25 13:21:44 +0200

    [add] add to ItemRepository methods findItemById,deleteById,save

--- src/main/java/com/mateusz/SystemERP/item/ItemRepository.java
+++ src/main/java/com/mateusz/SystemERP/item/ItemRepository.java
@@ -1,12 +1,19 @@
 package com.mateusz.SystemERP.item;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository {
 
     List<Item> findAll();
 
     List<Item> findItemsByProductId(Long id);
+
+    Optional<Item> findItemById(Long id);
+
+    Item save(Item item);
+
+    void deleteById(Long id);
 
     void addItemWithProductId(String material, int pieces, String quality, double weight, Long productId);
 
