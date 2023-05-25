fb195eeed433d08985da4e277c5ca35b4d2068f8
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-25 11:27:06 +0200

    [add] in CustomerService move from methods HttpStatus to controller and use exceptions

--- src/main/java/com/mateusz/SystemERP/customer/CustomerService.java
+++ src/main/java/com/mateusz/SystemERP/customer/CustomerService.java
@@ -2,56 +2,60 @@ package com.mateusz.SystemERP.customer;
 
 import com.mateusz.SystemERP.customer.Customer;
 import com.mateusz.SystemERP.customer.CustomerRepository;
+import com.mateusz.SystemERP.customer.exceptions.CustomerExistException;
+import com.mateusz.SystemERP.customer.exceptions.CustomerNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository repository;
 
-    public ResponseEntity<List<Customer>> getAllCustomers() {
+    public List<Customer> getAllCustomers() {
         List<Customer> customerList = repository.findAll();
         if (customerList.isEmpty()) {
-            return ResponseEntity
-                    .status(404)
-                    .build();
+            throw new CustomerNotFoundException("Customers list is empty.");
         }
-        return ResponseEntity
-                .status(200)
-                .body(customerList);
+        return customerList;
     }
 
-    public ResponseEntity<Customer> getCustomerByName(String name) {
-        if (repository.findCustomerByName(name).isEmpty()) {
-            return ResponseEntity
-                    .status(404)
-                    .build();
-        }
-        return ResponseEntity
-                .status(200)
-                .body(repository.findCustomerByName(name).get());
-
+    public Customer getCustomerByName(String name) {
+        return repository
+                .findCustomerByName(name)
+                .orElseThrow(() ->
+                        new CustomerNotFoundException("Customer with name " + name + " does not exist"));
     }
 
-    public ResponseEntity<?> addCustomer(String name) {
-        if (name.isBlank()) {
-            return ResponseEntity
-                    .status(400)
-                    .build();
+//    public ResponseEntity<Customer> addCustomer(String name) {
+//        if (name.isBlank()) {
+//            return ResponseEntity
+//                    .status(400)
+//                    .build();
+//        }
+//        if(repository.findCustomerByName(name).isPresent()){
+//            return ResponseEntity
+//                    .status(409)
+//                    .build();
+//        }
+//        repository.save(new Customer(name));
+//        return ResponseEntity
+//                .status(201)
+//                .build();
+//    }
+
+    public Customer addCustomer(String name) {
+        if (name.isBlank()){
+            throw new CustomerNotFoundException("Customer name is empty.");
         }
-        if(repository.findCustomerByName(name).isPresent()){
-            return ResponseEntity
-                    .status(409)
-                    .build();
+        if (repository.findCustomerByName(name).isPresent()){
+            throw new CustomerExistException("Customer with name " + name + " is already exist.");
         }
-        repository.save(new Customer(name));
-        return ResponseEntity
-                .status(201)
-                .build();
+        return repository.save(new Customer(name));
     }
 
 
