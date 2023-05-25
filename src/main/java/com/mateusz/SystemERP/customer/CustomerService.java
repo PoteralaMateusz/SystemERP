5b449db500c3a1639606ba1559dd324b6427782f
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-25 12:41:06 +0200

    [refactor] CustomerService delete comments and dont use imports

--- src/main/java/com/mateusz/SystemERP/customer/CustomerService.java
+++ src/main/java/com/mateusz/SystemERP/customer/CustomerService.java
@@ -1,15 +1,11 @@
 package com.mateusz.SystemERP.customer;
 
-import com.mateusz.SystemERP.customer.Customer;
-import com.mateusz.SystemERP.customer.CustomerRepository;
 import com.mateusz.SystemERP.customer.exceptions.CustomerExistException;
 import com.mateusz.SystemERP.customer.exceptions.CustomerNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,23 +26,6 @@ public class CustomerService {
                 .orElseThrow(() ->
                         new CustomerNotFoundException("Customer with name " + name + " does not exist"));
     }
-
-//    public ResponseEntity<Customer> addCustomer(String name) {
-//        if (name.isBlank()) {
-//            return ResponseEntity
-//                    .status(400)
-//                    .build();
-//        }
-//        if(repository.findCustomerByName(name).isPresent()){
-//            return ResponseEntity
-//                    .status(409)
-//                    .build();
-//        }
-//        repository.save(new Customer(name));
-//        return ResponseEntity
-//                .status(201)
-//                .build();
-//    }
 
     public Customer addCustomer(String name) {
         if (name.isBlank()){
