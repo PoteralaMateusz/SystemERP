ed82c1f1fa092d35c53e85050db9436d2db70e5e
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-27 15:36:55 +0200

    [add] create ItemDTOMapper

---
+++ src/main/java/com/mateusz/SystemERP/item/dto/ItemDTOMapper.java
@@ -0,0 +1,32 @@
+package com.mateusz.SystemERP.item.dto;
+
+import com.mateusz.SystemERP.item.Item;
+import com.mateusz.SystemERP.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ItemDTOMapper {
+    private final ItemRepository itemRepository;
+    public ItemDTO map(Item item){
+        return new ItemDTO(
+                item.getId(),
+                item.getMaterial(),
+                item.getQuality(),
+                item.getPieces(),
+                item.getWeight()
+        );
+    }
+
+    public Item map(ItemDTO itemDTO){
+        return new Item(
+                itemDTO.id(),
+                itemRepository.findItemById(itemDTO.id()).get().getProduct(),
+                itemDTO.material(),
+                itemDTO.quality(),
+                itemDTO.pieces(),
+                itemDTO.weight()
+                );
+    }
+}
