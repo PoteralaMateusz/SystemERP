0777252e4ddc6f4c224cad1f7733315d16e4e397
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-27 19:50:15 +0200

    [add] use ItemDTOMapper in ItemController, one method to save and update item

--- src/main/java/com/mateusz/SystemERP/item/ItemController.java
+++ src/main/java/com/mateusz/SystemERP/item/ItemController.java
@@ -1,41 +1,52 @@
 package com.mateusz.SystemERP.item;
 
+import com.mateusz.SystemERP.item.dto.ItemDTO;
+import com.mateusz.SystemERP.item.dto.ItemDTOMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService service;
+    private final ItemDTOMapper itemDTOMapper;
 
     @GetMapping("/items")
-    public ResponseEntity<List<Item>> findAllItems(){
+    public ResponseEntity<List<ItemDTO>> findAllItems(){
         return ResponseEntity
                 .status(200)
-                .body(service.findAllItems());
+                .body(service.findAllItems()
+                        .stream()
+                        .map(itemDTOMapper::map)
+                        .collect(Collectors.toList()));
     }
 
     @GetMapping("/items/{id}")
-    public ResponseEntity<List<Item>> findItemsByProductId(@PathVariable Long id) {
+    public ResponseEntity<List<ItemDTO>> findItemsByProductId(@PathVariable Long id) {
         return ResponseEntity
                 .status(200)
-                .body(service.findItemsByProductId(id));
+                .body(service.findItemsByProductId(id)
+                        .stream()
+                        .map(itemDTOMapper::map)
+                        .collect(Collectors.toList()));
     }
 
     @PostMapping("/items")
-    public ResponseEntity<Item> addItem( @RequestBody Item toAdd) {
+    public ResponseEntity<ItemDTO> addOrUpdateItem(@RequestBody ItemDTO toAdd) {
+        Item itemAdded = service.addItem(itemDTOMapper.map(toAdd));
         return ResponseEntity
                 .status(200)
-                .body(service.addItem(toAdd));
+                .body(itemDTOMapper.map(itemAdded));
     }
 
     @DeleteMapping("/items/{id}")
-    public ResponseEntity<Item> deleteItem(@PathVariable Long id){
+    public ResponseEntity<ItemDTO> deleteItem(@PathVariable Long id){
         return ResponseEntity
                 .status(200)
-                .body(service.deleteItemByID(id));
+                .body(itemDTOMapper.map(service.deleteItemByID(id)));
     }
 }
