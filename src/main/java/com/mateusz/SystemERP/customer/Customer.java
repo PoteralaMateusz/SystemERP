d01fb63758f6800786a6f2d30b659a0068359137
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-29 18:39:00 +0200

    [add] add field (country,city,street,houseNumber,zipCode) to Customer

--- src/main/java/com/mateusz/SystemERP/customer/Customer.java
+++ src/main/java/com/mateusz/SystemERP/customer/Customer.java
@@ -2,39 +2,31 @@ package com.mateusz.SystemERP.customer;
 
 import com.mateusz.SystemERP.order.Order;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Setter
+@Getter
+@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "customers")
 public class Customer {
 
     @Id
     private String name;
+    private String country;
+    private String city;
+    private String street;
+    private int houseNumber;
+    private int zipCode;
+
     @OneToMany(mappedBy = "customer")
+    @Transient
     private List<Order> orders;
 
-    public Customer(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    private List<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
 }
