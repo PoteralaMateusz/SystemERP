383c48f19e3d0b321e424548d3b6a2a872ab6fd1
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-29 18:41:47 +0200

    [add] use CustomerDTO in CustomerService and add addOrUpdateCustomer method

--- src/main/java/com/mateusz/SystemERP/customer/CustomerService.java
+++ src/main/java/com/mateusz/SystemERP/customer/CustomerService.java
@@ -1,40 +1,47 @@
 package com.mateusz.SystemERP.customer;
 
+import com.mateusz.SystemERP.customer.dto.CustomerDTO;
+import com.mateusz.SystemERP.customer.dto.CustomerDTOMapper;
 import com.mateusz.SystemERP.customer.exceptions.CustomerExistException;
 import com.mateusz.SystemERP.customer.exceptions.CustomerNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository repository;
+    private final CustomerDTOMapper customerDTOMapper;
 
-    public List<Customer> getAllCustomers() {
+    public List<CustomerDTO> getAllCustomers() {
         List<Customer> customerList = repository.findAll();
         if (customerList.isEmpty()) {
             throw new CustomerNotFoundException("Customers list is empty.");
         }
-        return customerList;
+        return customerList
+                .stream()
+                .map(customerDTOMapper::map)
+                .collect(Collectors.toList());
     }
 
-    public Customer getCustomerByName(String name) {
+    public CustomerDTO getCustomerByName(String name) {
         return repository
                 .findCustomerByName(name)
+                .map(customerDTOMapper::map)
                 .orElseThrow(() ->
                         new CustomerNotFoundException("Customer with name " + name + " does not exist"));
     }
 
-    public Customer addCustomer(String name) {
-        if (name.isBlank()){
-            throw new CustomerNotFoundException("Customer name is empty.");
+    @Transactional
+    public CustomerDTO addOrUpdateCustomer(CustomerDTO toSave) {
+        if (toSave == null){
+            throw new CustomerNotFoundException("Customer data is null.");
         }
-        if (repository.findCustomerByName(name).isPresent()){
-            throw new CustomerExistException("Customer with name " + name + " is already exist.");
-        }
-        return repository.save(new Customer(name));
+        return customerDTOMapper.map(repository.save(customerDTOMapper.map(toSave)));
     }
 
 
