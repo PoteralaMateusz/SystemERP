56d730cf623d1511e8b2b9618bcd2f32647f5de9
MateuszPoterala <poteralamateusz@gmail.com>
2023-05-30 16:54:39 +0200

    [refactor] move mapper from ItemController to ItemService

--- src/main/java/com/mateusz/SystemERP/item/ItemController.java
+++ src/main/java/com/mateusz/SystemERP/item/ItemController.java
@@ -1,52 +1,43 @@
 package com.mateusz.SystemERP.item;
 
 import com.mateusz.SystemERP.item.dto.ItemDTO;
-import com.mateusz.SystemERP.item.dto.ItemDTOMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService service;
-    private final ItemDTOMapper itemDTOMapper;
+
 
     @GetMapping("/items")
-    public ResponseEntity<List<ItemDTO>> findAllItems(){
+    public ResponseEntity<List<ItemDTO>> findAllItems() {
         return ResponseEntity
                 .status(200)
-                .body(service.findAllItems()
-                        .stream()
-                        .map(itemDTOMapper::map)
-                        .collect(Collectors.toList()));
+                .body(service.findAllItems());
     }
 
     @GetMapping("/items/{id}")
     public ResponseEntity<List<ItemDTO>> findItemsByProductId(@PathVariable Long id) {
         return ResponseEntity
                 .status(200)
-                .body(service.findItemsByProductId(id)
-                        .stream()
-                        .map(itemDTOMapper::map)
-                        .collect(Collectors.toList()));
+                .body(service.findItemsByProductId(id));
     }
 
     @PostMapping("/items")
     public ResponseEntity<ItemDTO> addOrUpdateItem(@RequestBody ItemDTO toAdd) {
-        Item itemAdded = service.addItem(itemDTOMapper.map(toAdd));
         return ResponseEntity
                 .status(200)
-                .body(itemDTOMapper.map(itemAdded));
+                .body(service.addItem(toAdd));
     }
 
     @DeleteMapping("/items/{id}")
-    public ResponseEntity<ItemDTO> deleteItem(@PathVariable Long id){
+    public ResponseEntity<ItemDTO> deleteItem(@PathVariable Long id) {
         return ResponseEntity
                 .status(200)
-                .body(itemDTOMapper.map(service.deleteItemByID(id)));
+                .body(service.deleteItemByID(id));
     }
 }
--- src/main/java/com/mateusz/SystemERP/item/ItemService.java
+++ src/main/java/com/mateusz/SystemERP/item/ItemService.java
@@ -1,55 +1,67 @@
 package com.mateusz.SystemERP.item;
 
+import com.mateusz.SystemERP.item.dto.ItemDTO;
+import com.mateusz.SystemERP.item.dto.ItemDTOMapper;
 import com.mateusz.SystemERP.item.exceptions.ItemNotFoundException;
 import com.mateusz.SystemERP.product.Product;
 import com.mateusz.SystemERP.product.ProductRepository;
+import com.mateusz.SystemERP.product.exceptions.ProductNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
     private final ProductRepository productRepository;
+    private final ItemDTOMapper itemDTOMapper;
 
-    public List<Item> findAllItems() {
+    public List<ItemDTO> findAllItems() {
         List<Item> itemsList = itemRepository.findAll();
         if (itemsList.isEmpty()) {
             throw new ItemNotFoundException("Items list is empty");
         }
-        return itemsList;
+        return itemsList
+                .stream()
+                .map(itemDTOMapper::map)
+                .collect(Collectors.toList());
     }
 
-    public List<Item> findItemsByProductId(Long id) {
+    public List<ItemDTO> findItemsByProductId(Long id) {
         List<Item> itemsByProductId = itemRepository.findItemsByProductId(id);
         if (itemsByProductId.isEmpty()) {
             throw new ItemNotFoundException("Not found items that have product id: " + id + ".");
         }
-        return itemsByProductId;
+        return itemsByProductId
+                .stream()
+                .map(itemDTOMapper::map)
+                .collect(Collectors.toList());
     }
 
     @Transactional
-    public Item addItem(Item toAdd) {
-        Optional<Product> product = productRepository.findProductById(toAdd.getProduct().getId());
+    public ItemDTO addItem(ItemDTO itemDTOtoAdd) {
+        Optional<Product> product = productRepository.findProductById(itemDTOtoAdd.productId());
         if (product.isEmpty()) {
-            productRepository.save(toAdd.getProduct());
+            throw new ProductNotFoundException("Product with id " + itemDTOtoAdd.productId() + " does not exist.");
         }
-        product.ifPresent(toAdd::setProduct);
-        itemRepository.save(toAdd);
-        return toAdd;
+        Item itemToAdd = itemDTOMapper.map(itemDTOtoAdd);
+        itemToAdd.setProduct(product.get());
+        itemRepository.save(itemToAdd);
+        return itemDTOMapper.map(itemToAdd);
 
     }
 
-    public Item deleteItemByID(Long id){
+    public ItemDTO deleteItemByID(Long id) {
         Optional<Item> itemToDelete = itemRepository.findItemById(id);
-        if (itemToDelete.isEmpty()){
+        if (itemToDelete.isEmpty()) {
             throw new ItemNotFoundException("Item with " + id + " id not found.");
         }
         itemRepository.deleteById(itemToDelete.get().getId());
-        return itemToDelete.get();
+        return itemDTOMapper.map(itemToDelete.get());
     }
 }
