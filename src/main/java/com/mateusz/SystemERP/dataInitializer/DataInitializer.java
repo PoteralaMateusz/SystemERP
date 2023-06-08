79f33189171df81f2be93660e4e74647c7077982
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-08 14:24:30 +0200

    [change] change in DataInitializer

--- src/main/java/com/mateusz/SystemERP/dataInitializer/DataInitializer.java
+++ src/main/java/com/mateusz/SystemERP/dataInitializer/DataInitializer.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 @Component
@@ -21,30 +21,30 @@ public class DataInitializer {
     private final ProductRepository productRepository;
 
     @PostConstruct
-    public void initData(){
+    public void initData() {
         customersInit();
         ordersInit();
     }
 
-    private void customersInit(){
-        Customer customer1 = new Customer("MetalBud","Poland","Warszawa","Mickiewicza",14,12345,null);
-        Customer customer2 = new Customer("BridgeBuilding","England","London","Main",123,12345,null);
+    private void customersInit() {
+        Customer customer1 = new Customer("MetalBud", "Poland", "Warszawa", "Mickiewicza", 14, 12345, null);
+        Customer customer2 = new Customer("BridgeBuilding", "England", "London", "Main", 123, 12345, null);
         customerRepository.save(customer1);
         customerRepository.save(customer2);
     }
 
-    private void ordersInit(){
+    private void ordersInit() {
         Order order1 = new Order(
                 null,
                 null,
                 "2023-005",
-                LocalDateTime.now().minusDays(10),
-                LocalDateTime.now().plusDays(36),
-                LocalDateTime.now(),
+                LocalDate.now().minusDays(10),
+                LocalDate.now().plusDays(36),
+                LocalDate.now(),
                 BigDecimal.valueOf(12500L),
                 new ArrayList<>()
         );
-        orderRepository.addOrderWithCustomerId(order1.getOrderNumber(),order1.getDeadline(),order1.getFinishDate(),order1.getOrderDate(),order1.getPrice(),"MetalBud");
+        orderRepository.addOrderWithCustomerId(order1.getOrderNumber(), order1.getDeadline(), order1.getFinishDate(), order1.getOrderDate(), order1.getPrice(), "MetalBud");
 
     }
 
