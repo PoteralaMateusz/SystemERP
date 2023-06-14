1f7a72bfb79a4c426f3c467231531957ef2afb76
MateuszPoterala <poteralamateusz@gmail.com>
2023-06-14 18:35:09 +0200

    [add] create OrderStatsDTO

---
+++ src/main/java/com/mateusz/SystemERP/order/dto/OrderStatsDTO.java
@@ -0,0 +1,12 @@
+package com.mateusz.SystemERP.order.dto;
+
+public record OrderStatsDTO(
+        Long id,
+        String customerName,
+        String orderNumber,
+        Long daysToDeadline,
+        Double orderWeight,
+        Double leftWeight,
+        Double workProgress
+) {
+}
